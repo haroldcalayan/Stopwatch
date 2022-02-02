1da416ba2bb3158eefb6d8aa086621d3782e39ec
Harold Calayan <haroldcalayan@gmail.com>
2022-02-02 20:58:09 +0800

    :hammer: Move main activity inside ui/main package

--- app/src/main/java/com/haroldcalayan/stopwatch/ui/main/MainActivity.kt
+++ app/src/main/java/com/haroldcalayan/stopwatch/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.haroldcalayan.stopwatch
+package com.haroldcalayan.stopwatch.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haroldcalayan.stopwatch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
