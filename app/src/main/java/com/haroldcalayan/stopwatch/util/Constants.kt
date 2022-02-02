e4131e04f69d7f862f7f3cd4c169b3bc172acdda
Harold Calayan <haroldcalayan@gmail.com>
2022-02-02 21:22:25 +0800

    :sparkles: Create constants and utils

---
+++ app/src/main/java/com/haroldcalayan/stopwatch/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.haroldcalayan.stopwatch.util
+
+object Constants {
+
+    const val ONE_SECOND = 1000L
+    const val ONE_MINUTE = 60 * ONE_SECOND
+
+    const val SPLASH_SCREEN_LIFE = 3 * ONE_SECOND
+}
